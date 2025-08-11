04ce61dcd12b7050b6a5b7401296bc58aab1ce53
Mateo Ortega <mateo041ortega@gmail.com>
2025-08-10 20:26:05 -0700

    Saving code

---
+++ src/camera/cam_utils/simd_helper.rs
@@ -0,0 +1,11 @@
+
+// use std::simd::{f32x8, u8x8, StdFloat};
+//
+//
+// pub fn opaque_to_f32_normalized( data: &[u8], w: usize , h:usize) -> Vec<f32> {
+//     let pixel_count = w * h;
+//     let mut chw_data = vec![0.0;pixel_count * 3];
+//
+//     // let inv_255 = f32x8
+//     chw_data
+// }
