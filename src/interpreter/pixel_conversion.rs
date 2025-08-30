dcc748f6ec6fee09bf4a4d09f21c1741ab7a4d67
Mateo Ortega <mateo041ortega@gmail.com>
2025-08-29 21:47:57 -0700

    Adding new pixel trait

---
+++ src/interpreter/pixel_conversion.rs
@@ -0,0 +1,29 @@
+use tract_onnx::prelude::{tract_data::half, Datum};
+
+
+
+pub trait FromPixel :Sized {
+    fn from_pixel(p: u8) -> Self;
+}
+
+
+impl FromPixel for f32 {
+    #[inline]
+    fn from_pixel(p : u8)->Self {
+        p as f32 / 255.0
+    }
+}
+
+impl FromPixel for half::f16{
+    #[inline]
+    fn from_pixel(p : u8)->Self {
+        half::f16::from_f32(p as f32 / 255.0)
+    }
+}
+
+impl FromPixel for u8{
+    #[inline]
+    fn from_pixel(p : u8) ->Self {
+        p
+    }
+}
